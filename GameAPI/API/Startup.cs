8862608f679597f21a814cb4602b71b426ecdbc5
syoung908 <syoung908@gmail.com>
2021-09-13 10:33:35 -0700

    Client Scaffold

--- GameAPI/API/Startup.cs
+++ GameAPI/API/Startup.cs
@@ -22,7 +22,7 @@ namespace API {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-
+            services.AddSignalR();
             services.AddControllers();
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
